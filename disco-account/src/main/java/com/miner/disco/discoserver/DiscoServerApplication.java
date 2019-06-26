4bc9e244d654b9ee05e941e9390b05d9e7b8c688
wangz <786917861@qq.com>
2019-06-26 12:25:08 +0800

    新增模块

---
+++ disco-account/src/main/java/com/miner/disco/discoserver/DiscoServerApplication.java
@@ -0,0 +1,13 @@
+package com.miner.disco.discoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoServerApplication.class, args);
+    }
+
+}
